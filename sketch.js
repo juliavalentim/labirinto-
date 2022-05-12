5dae72c68250e8282cd39f19d20f6c98c9321cfb
juliavalentim <julia.s.valentim12@gmail.com>
2022-05-11 21:02:22 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,8 @@
 var player;
+var wallGroup;
+var key1
+var key2
+var wall26, wall27
 
 function preload(){
 
@@ -8,6 +12,12 @@ function setup(){
 createCanvas(750,650)
 
 player = createSprite(42,20,38,30)
+key1 = createSprite(520,190,25,20)
+key2 = createSprite(680,510,25,20)
+wall26 = createSprite(270,230,15,80)
+wall27 = createSprite(290,610,15,80)
+
+wallGroup = new Group()
 
 }
 
@@ -15,19 +25,34 @@ function draw(){
     background("green")
 
     if (keyDown("right")) {
-        player.x += 2
+        player.x += 4
     }
 
     if (keyDown("left")) {
-        player.x -= 2
+        player.x -= 4
     }
 
     if (keyDown("up")) {
-        player.y -= 2
+        player.y -= 4
     }
 
     if (keyDown("down")) {
-        player.y += 2
+        player.y += 4
+    }
+
+    if(player.isTouching(wallGroup)){
+        player.x = 42
+        player.y = 20
+    }
+
+    if(player.isTouching(key1)){
+        wall26.destroy()
+        
+    }
+
+    if(player.isTouching(key2)){
+        wall27.destroy()
+        
     }
 
     text(mouseX + "-" + mouseY, mouseX, mouseY)
@@ -62,5 +87,32 @@ function wall(){
     var wall22 = createSprite(206,270,200,15)
     var wall23 = createSprite(100,413,15,302)
     var wall25 = createSprite(500,572,650,15)
-    var wall26 = createSprite(183,450,15,230)
+    var wall24 = createSprite(183,450,15,230)
+    wallGroup.add(wall1)
+    wallGroup.add(wall2)
+    wallGroup.add(wall3)
+    wallGroup.add(wall4)
+    wallGroup.add(wall5)
+    wallGroup.add(wall6)
+    wallGroup.add(wall7)
+    wallGroup.add(wall8)
+    wallGroup.add(wall9)
+    wallGroup.add(wall10)
+    wallGroup.add(wall11)
+    wallGroup.add(wall12)
+    wallGroup.add(wall13)
+    wallGroup.add(wall14)
+    wallGroup.add(wall15)
+    wallGroup.add(wall16)
+    wallGroup.add(wall17)
+    wallGroup.add(wall18)
+    wallGroup.add(wall19)
+    wallGroup.add(wall20)
+    wallGroup.add(wall21)
+    wallGroup.add(wall22)
+    wallGroup.add(wall23)
+    wallGroup.add(wall24)
+    wallGroup.add(wall25)
+    
+    
 }
